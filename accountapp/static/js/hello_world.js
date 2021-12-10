db234cc1d391ea6b1d868a17d273e3005b4e094c
jh_2 <success899@naver.com>
2021-12-10 14:55:14 +0900

    HTTP POST model list visualization

--- accountapp/static/js/hello_world.js
+++ accountapp/static/js/hello_world.js
@@ -9,6 +9,9 @@ function send_input() {
 
         document.getElementById('text').innerHTML = response.data['text'];   // 입력받은 text를 찾음
         document.getElementById('new_model_created_at').innerHTML = response.data['created_at'];
+
+        document.getElementById('new_model_list').innerHTML += "<h5>" + response.data['text'] + "</h5>";
+        document.getElementById('new_model_list').innerHTML += "<p>" + response.data['created_at'] + "</p>";
       })
       .catch(function (error) {
         console.log(error);
