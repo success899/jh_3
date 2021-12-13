cc2d1094f2ed3d572db0b58b234d6ad75a9003a4
jh_2 <success899@naver.com>
2021-12-13 13:44:14 +0900

    Account Login template view create

---
+++ accountapp/static/js/login.js
@@ -0,0 +1,45 @@
+
+function send_input() {
+    axios.post('/accounts/login/', {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value,
+      })
+      .then(function (response) {
+        // POST 요청이 성공했을 경우 어떤 javascript 가 구동되어야 하는지에 대한 부분
+        console.log(response);
+
+        document.getElementById('alert_box').innerHTML = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다.</div>";
+
+        window.location.href = '/accounts/hello_world_template/';
+
+      })
+      .catch(function (error) {
+        // POST 요청이 실패했을 경우 어떤 javascript 가 구동되어야 하는지에 대한 부분
+        console.log(error);
+
+        document.getElementById('alert_box').innerHTML = "<div class='btn btn-danger rounded-pill px-5'>로그인이 실패했습니다.</div>"
+      });
+}
+
+
+
+<!-- 서버 호출 -->
+axios.get('/accounts/hello_world/')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+
+    // 성공했을 경우 어떤 javascript 작업을 할지
+    for (let i=0; i < response.data.length; i++) {
+        document.getElementById('new_model_list').innerHTML += "<h5>" + response.data[i]['text'] + "</h5>";
+        document.getElementById('new_model_list').innerHTML += "<p>" + response.data[i]['created_at'] + "</p>";
+    }
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
-from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView, ListUsers
+from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView, \
+    AccountLoginView
 
 app_name = 'accountapp'
 
@@ -11,11 +12,10 @@ urlpatterns = [
     # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
 
-    #토큰을 가져오기 위한 부분
+
+    path('login_template/', AccountLoginView, name='login_template'),
     path('login/', views.obtain_auth_token, name='login'),
 
-    # 유저 전체 리스트를 확인하는 부분
-    path('list/', ListUsers.as_view(), name='list'),
 
     path('create_template/', AccountCreateTemplate, name='create_template'),
     path('create/', AccountCreateAPIView.as_view(), name='create'),
