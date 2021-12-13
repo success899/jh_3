518815042b5b2129fbb49adb24b48c26144a329e
jh_2 <success899@naver.com>
2021-12-13 11:22:04 +0900

    obtain token view update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
 from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView
 
@@ -9,6 +10,11 @@ urlpatterns = [
     path('hello_world_template/', hello_world_template, name='hello_world_template'),
     # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
+
+    #토큰을 가져오기 위한 부분
+    path('login/', views.obtain_auth_token, name='login'),
+
+
 
     path('create_template/', AccountCreateTemplate, name='create_template'),
     path('create/', AccountCreateAPIView.as_view(), name='create'),
