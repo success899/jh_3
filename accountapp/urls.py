935392f63cfffbd9882e40043db02df266093ad3
jh_2 <success899@naver.com>
2021-12-13 13:15:41 +0900

    Account list view testing

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
-from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView
+from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView, ListUsers
 
 app_name = 'accountapp'
 
@@ -14,7 +14,8 @@ urlpatterns = [
     #토큰을 가져오기 위한 부분
     path('login/', views.obtain_auth_token, name='login'),
 
-
+    # 유저 전체 리스트를 확인하는 부분
+    path('list/', ListUsers.as_view(), name='list'),
 
     path('create_template/', AccountCreateTemplate, name='create_template'),
     path('create/', AccountCreateAPIView.as_view(), name='create'),
