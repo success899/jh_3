04e5248dd428de66cd9d5aa358e83028f6db71c5
jh_2 <success899@naver.com>
2021-12-14 14:53:11 +0900

    update delete API permission update 2

--- accountapp/permissions.py
+++ accountapp/permissions.py
@@ -3,11 +3,6 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
-    """
-    Allows access only to admin users.
-    """
 
-    def has_permission(self, request, view):
-        user = User.objects.get(pk=view.kwargs['pk'])
-
-        return request.user == user
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
