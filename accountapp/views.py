bcc565e975d0da617bba1536a407123a5c9d8c8a
jh_2 <success899@naver.com>
2021-12-14 16:34:35 +0900

    REST architecture update

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,10 +6,9 @@ from django.views.generic import TemplateView
 from rest_framework import authentication, permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view
-from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, \
+    RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-
-# UI 설정부분
 from rest_framework.views import APIView
 
 from accountapp.models import NewModel
@@ -17,6 +16,7 @@ from accountapp.permissions import IsOwner
 from accountapp.serializers import NewModelSerializer, UserSerializer, UserWithoutPasswordSerializer
 
 
+# UI 설정부분
 def hello_world_template(request):
     return render(request, 'accountapp/hello_world.html')
 
@@ -37,7 +37,7 @@ def hello_world(request):
         return Response(serializer.data)
 
     new_model_list = NewModel.objects.all()
-    serializer = NewModelSerializer(new_model_list, many=True)      # 다수의 객체 serializer 시켜주는 부분
+    serializer = NewModelSerializer(new_model_list, many=True)
 
     return Response(serializer.data)
 
@@ -51,34 +51,26 @@ class AccountCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = []
 
+
 def AccountLoginView(request):
     return render(request, 'accountapp/login.html')
 
-class AcountRetrieveTemplateView(TemplateView):
+
+class AccountRetrieveTemplateView(TemplateView):
     template_name = 'accountapp/retrieve.html'
 
-class AccountRetrieveAPIView(RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserWithoutPasswordSerializer
-
-    permission_classes = [permissions.AllowAny]
-    authentication_classes = [TokenAuthentication]
 
 class AccountUpdateTemplateView(TemplateView):
     template_name = 'accountapp/update.html'
 
-class AccountUpdateAPIView(UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserWithoutPasswordSerializer
-
-    permission_classes = [IsOwner]
-    authentication_classes = [TokenAuthentication]
 
 class AccountDestroyTemplateView(TemplateView):
     template_name = 'accountapp/destroy.html'
 
-class AccountDestroyAPIView(DestroyAPIView):
+
+class AccountRUDAPIView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
+    serializer_class = UserWithoutPasswordSerializer
 
     permission_classes = [IsOwner]
     authentication_classes = [TokenAuthentication]
